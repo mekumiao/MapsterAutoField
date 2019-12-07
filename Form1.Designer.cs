a97fe0c3a172f75c9cc57768a165f05ec97d10bf
华 繁 <2319002545@qq.com>
2019-12-07 17:13:46 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace MapsterAutoField
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem(new string[] {
+            "name",
+            "name",
+            "1"}, -1);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.cldest = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clsrc = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.srctype = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtSrcType = new System.Windows.Forms.TextBox();
+            this.txtDestType = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtDest = new System.Windows.Forms.TextBox();
+            this.txtSrc = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.open = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "源类型";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(199, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "目标类型";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.cldest,
+            this.clsrc,
+            this.srctype});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem3});
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(688, 562);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listView1_KeyDown);
+            // 
+            // cldest
+            // 
+            this.cldest.Text = "目标字段";
+            this.cldest.Width = 200;
+            // 
+            // clsrc
+            // 
+            this.clsrc.Text = "源字段";
+            this.clsrc.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.clsrc.Width = 200;
+            // 
+            // srctype
+            // 
+            this.srctype.Text = "类型 (1：字段，2：默认值)";
+            this.srctype.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.srctype.Width = 200;
+            // 
+            // txtSrcType
+            // 
+            this.txtSrcType.Location = new System.Drawing.Point(61, 12);
+            this.txtSrcType.Name = "txtSrcType";
+            this.txtSrcType.Size = new System.Drawing.Size(100, 21);
+            this.txtSrcType.TabIndex = 2;
+            this.txtSrcType.Text = "crej_cj";
+            // 
+            // txtDestType
+            // 
+            this.txtDestType.Location = new System.Drawing.Point(258, 12);
+            this.txtDestType.Name = "txtDestType";
+            this.txtDestType.Size = new System.Drawing.Size(100, 21);
+            this.txtDestType.TabIndex = 3;
+            this.txtDestType.Text = "creservem_cr";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.txtType);
+            this.splitContainer1.Panel1.Controls.Add(this.btnImport);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.txtDest);
+            this.splitContainer1.Panel1.Controls.Add(this.txtSrcType);
+            this.splitContainer1.Panel1.Controls.Add(this.txtSrc);
+            this.splitContainer1.Panel1.Controls.Add(this.txtDestType);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(1336, 723);
+            this.splitContainer1.SplitterDistance = 157;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // txtType
+            // 
+            this.txtType.Location = new System.Drawing.Point(498, 117);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(56, 21);
+            this.txtType.TabIndex = 9;
+            this.txtType.Text = "1";
+            this.txtType.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtType_KeyDown);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(16, 88);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 23);
+            this.btnImport.TabIndex = 10;
+            this.btnImport.Text = "导入";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(692, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "生成";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtDest
+            // 
+            this.txtDest.Location = new System.Drawing.Point(16, 117);
+            this.txtDest.Name = "txtDest";
+            this.txtDest.Size = new System.Drawing.Size(185, 21);
+            this.txtDest.TabIndex = 4;
+            this.txtDest.Text = "cm_cid";
+            this.txtDest.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox3_KeyDown);
+            // 
+            // txtSrc
+            // 
+            this.txtSrc.Location = new System.Drawing.Point(223, 117);
+            this.txtSrc.Name = "txtSrc";
+            this.txtSrc.Size = new System.Drawing.Size(186, 21);
+            this.txtSrc.TabIndex = 5;
+            this.txtSrc.Text = "cj_ccustid";
+            this.txtSrc.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox4_KeyDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(456, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "类型";
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.listView1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.richTextBox1);
+            this.splitContainer2.Size = new System.Drawing.Size(1336, 562);
+            this.splitContainer2.SplitterDistance = 688;
+            this.splitContainer2.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(644, 562);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1336, 723);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader cldest;
+        private System.Windows.Forms.ColumnHeader clsrc;
+        private System.Windows.Forms.ColumnHeader srctype;
+        private System.Windows.Forms.TextBox txtSrcType;
+        private System.Windows.Forms.TextBox txtDestType;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox txtDest;
+        private System.Windows.Forms.TextBox txtSrc;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtType;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.OpenFileDialog open;
+    }
+}
+
